003e3121b5b066bf514b7bdee5dc6921ced08d9a
이름 <>
2021-04-27 23:24:47 +0900

    백신센터 맵에 표출

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const searchWrap = document.querySelector(".searchWrap");
 const searchButton = document.querySelector(".searchButton");
 const currentLocation = document.querySelector(".currentLocation");
 const searchLocation = document.querySelector(".searchLocation");
+const topBar = document.querySelector(".topBar");
+const searchLocationOnMap = document.querySelector(".searchLocationOnMap");
+const searchButtonOnMap = document.querySelector(".searchButtonOnMap");
 
 
 const mapContainer = document.querySelector("#map");
 let locationValue = "";
 
 
-// 주소검색으로 이동
-searchButton.addEventListener("click", e => {
-    locationValue = searchLocation.value;
-    eventHandle(e);
-
+// 지역검색, 주소=>좌표 변환 함수
+function findCoords(locationValue) {
     naver.maps.Service.geocode({
         query : locationValue
     }, function(status, response) {
@@ -23,12 +23,20 @@ searchButton.addEventListener("click", e => {
         }
         if(response.v2.meta.totalCount === 0) return alert("결과가 검색되지 않음");
 
-        console.log(response);
         let item = response.v2.addresses[0],
             point = new naver.maps.Point(item.x, item.y);
 
             map.setCenter(point);
     });
+}
+
+
+// 주소검색으로 이동
+searchButton.addEventListener("click", e => {
+    locationValue = searchLocation.value;
+    eventHandle(e);
+
+    findCoords(locationValue);
 })
 
 // 현재위치로 이동
@@ -42,25 +50,56 @@ currentLocation.addEventListener("click" , e => {
     });
 })
 
+// 맵상에서 주소검색
+searchButtonOnMap.addEventListener("click", e => {
+    e.preventDefault();
+    locationValue = searchLocationOnMap.value;
+
+    findCoords(locationValue);
+})
+
+// 이벤트핸들(화면넘김막기)
 function eventHandle(e) {
     e.preventDefault();
     searchWrap.style.display = "none";
     mapContainer.style.display = "block";
-}
+    topBar.style.display = "block";
+}   
 
-let mapOptions = {zoom: 15};
+let mapOptions = {zoom: 13};
 // 맵 생성
 let map = new naver.maps.Map('map', mapOptions);
+let markerList = [];
 
 
-const url = "https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=204&" + 
-    "serviceKey=gNWiWkY9fyJ3dGm%2FIwxImL9MOl930bOHlSItvNS%2BPPZVQZuouadlFyn5sLYy9HEFkGLcEanPhYPDM8uN9f6j1w%3D%3D";
 
+const url = "https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=1000&" + 
+"serviceKey=gNWiWkY9fyJ3dGm%2FIwxImL9MOl930bOHlSItvNS%2BPPZVQZuouadlFyn5sLYy9HEFkGLcEanPhYPDM8uN9f6j1w%3D%3D";
+let centerList = null;
+//공공데이터포털 api 참조, 백신센터조회
 async function getCenterList() {
     let response = await fetch(url);
-    if(response.ok){
-        return await response.json(); 
-    } else {
-        alert("HTTP-Error: " + response.status);
-    }
+    let result = await response.json();
+    markerList = createMarker(result.data);
 }
+
+
+
+function createMarker(data){
+    data.forEach(element => {
+        let marker = new naver.maps.Marker({
+            position: new naver.maps.LatLng(element.lng, element.lat),
+            map: map,
+            icon: {
+                url: "syringe1.png",
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35)
+            }
+        })
+        markerList.push(marker);
+    })
+    console.log(markerList);
+}
+
+getCenterList();
+
